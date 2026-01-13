18eb79810055a91690981642e49819588bb41a61
Ashahab007 <ashahab007@gmail.com>
2026-01-14 03:11:10 +0600

    updated typecheck for fixing error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./Component/Navbar";
-import Footer from "./Component/Footer";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
